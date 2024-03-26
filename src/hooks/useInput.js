26fb7817979719db1c515d0f926ef939d5f87142
abdulaziz <abdul.umar.azis@gmail.com>
2024-03-26 17:13:59 +0700

    make test error

--- src/hooks/useInput.js
+++ src/hooks/useInput.js
@@ -7,7 +7,7 @@ function useInput(defaultValue = '') {
     setValue(event.target.value);
   };
 
-  return [value, onValueChangeHandler, setValue];
+  return [value, setValue];
 }
 
 export default useInput;
